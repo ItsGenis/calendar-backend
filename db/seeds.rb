e901549b303c2870c8c82b59afd4521cf55f9bae
Genís <genis.matutes@gmail.com>
2022-12-28 17:25:19 +0100

    feat: add events

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Event.create!([
+  {
+    title: 'Event 1',
+    description: 'First Event',
+    starts_at: DateTime.now,
+    ends_at: DateTime.now + 1.hour,
+  },
+  {
+    title: 'Event 2',
+    description: 'First Event',
+    starts_at: DateTime.now - 1.days,
+    ends_at: DateTime.now + 5.days
+  },
+  {
+    title: 'Event 3',
+    description: 'First Event',
+    starts_at: DateTime.now + 3.days,
+    ends_at: DateTime.now + 12.days
+  }
+])
